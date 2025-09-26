6d94083087041449af5160b3ad933d0586a14b2c
swapnil sahu <swapnilvp846@gmail.com>
2025-09-26 21:22:24 +0530

    added zod for type validation

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -1,5 +1,30 @@
-export interface User {
-    id: number;
-    name: string;
-    email: string;
-}
+import z from "zod"
+const SignupRequestSchema = z.object({
+    email: z.string(),
+    password: z.string(),
+    name: z.string()
+})
+
+const UserSchema = z.object({
+    email: z.string,
+    password: z.string,
+    name: z.string
+})
+
+const JwtPayloadSchema = z.object({
+    token: z.string(),
+    userId: z.string(),
+    name: z.string(),
+    jwtId: z.string(),
+    iat: z.number(),
+    ipAddress: z.string(),
+    deviceInfo: z.string(),
+    issuedAt: z.number(),
+    expiresIn: z.number(),
+})
+
+type User = z.infer<typeof UserSchema>;
+type Signup = z.infer<typeof SignupRequestSchema>;
+type JwtPayloadType = z.infer<typeof JwtPayloadSchema>
+
+export { UserSchema, type User, SignupRequestSchema, type Signup, JwtPayloadSchema, type JwtPayloadType }
