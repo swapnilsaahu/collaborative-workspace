a58b27399b9ded378a830e61ce081b2ad254e28c
swapnil sahu <swapnilvp846@gmail.com>
2025-10-09 22:12:39 +0530

    websocket logic and whiteboard

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,11 +1,16 @@
 import express, { Application, Request, Response } from 'express';
 import dotenv from "dotenv";
-
+import http from "node:http";
+import { WebSocketServer, Server, WebSocket } from "ws";
 dotenv.config();
+
+
+
+
 const app: Application = express();
 const port = 3000;
 
-
+const httpServer = http.createServer(app);
 
 app.use(express.json());
 
@@ -20,5 +25,46 @@ app.get('/', (req: Request, res: Response) => {
 app.listen(port, () => {
     console.log('server listening at port', port);
 })
+const handleUnkownError = (ws: WebSocket) => {
+    ws.send(JSON.stringify(
+        {
+            type: 'error',
+            message: "not a valid request for websocket"
+        }
+    ));
+}
+const webSocketServer = () => {
+    const wss = new WebSocketServer({ server: httpServer });
+
+    wss.on('connection', (ws) => {
+        ws.on('error', console.error);
+
+        const connectionMapping = {
+            "join": joinWhtieboard,
+            "sendMessage": sendMessage,
+        }
+        ws.on('error', console.error);
+
+
+        // Handle messages from the user
+        ws.on("message", (data) => {
+            try {
+                const msg = JSON.parse(data);
+                const { type } = msg;
+                console.log(type, "on message");
+                const handler = connectionMapping[type]
+                if (handler) {
+                    handler(ws, msg);
+                }
+                else {
+                    handleUnkownError(ws);
+                }
+            }
+            catch (error) {
+                handleUnkownError(ws);
+            }
+        });
+    })
+}
 
 export default app;
